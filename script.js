3d1190500b934bbec206a24c6b5f9e58fe7eec7c
aleguimaraes <91500883+aleguimaraes@users.noreply.github.com>
2021-09-27 17:42:35 -0300

    adicionei os arquivos do certificard

---
+++ script.js
@@ -0,0 +1,8 @@
+// body: engloba todas as coisas que iremos usar
+// div: faz uma divisao na escrita 
+// ul: lista nao ordenada e ol: lista ordenada 
+// ancora: clicar no link
+
+function mudaTema() {
+  document.body.classList.toggle("dark");  
+}
